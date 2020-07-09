a3b437e4714615c06fa9c7122b10e1c0bb27e05a
krzys_h <krzys_h@interia.pl>
2020-07-09 23:55:33 +0200

    GitHub IDE is the best IDE

--- projektor.py
+++ projektor.py
@@ -121,13 +121,13 @@ async def on_message(message):
 
     if "kaczki" in message.content.lower():
         await message.add_reaction("🦆")
-        if client.user in message.mention:
+        if client.user in message.mentions:
             msg = await projector_channel.send(file=discord.File("./quack.mp4-q38Y5FLK63k.mp4"))
             await msg.add_reaction("🦆")
             
     if any(wordsearch(w, message.content) for w in ('kurki', 'kurkom', 'jajka', 'farmio', 'kur', 'GMO')):
         await message.add_reaction("🥚")
-        if client.user in message.mention:
+        if client.user in message.mentions:
             msg = await projector_channel.send(file=discord.File(os.path.join("./farmio", random.choice(os.listdir("./farmio")))))
             await msg.add_reaction("🦆")
 
